1bd758a71bdffd8f0e55b8a5b25c4ccf60a45f8c
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-07 15:47:51 -0500

    creating interface and styling, adding the google maps frame

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function initMap() {
+  var la = {
+        lat: 34.052235,
+        lng: -118.243683
+    };
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {center: la, zoom:14});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: la, map: map});
+}
