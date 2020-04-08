42862bc3ac1a6b72367dc0bf49a818dfbaa868b1
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-07 23:59:13 -0500

    adding the store container dynamically, loading the stores json into index js

--- js/index.js
+++ js/index.js
@@ -10,4 +10,31 @@ function initMap() {
       document.getElementById('map'), {center: la, zoom:14});
   // The marker, positioned at Uluru
   var marker = new google.maps.Marker({position: la, map: map});
+  addressLocation();
+}
+
+function addressLocation() {
+    stores.map((store) => {
+        console.log("piece");
+    })
+    
+    let storeList = document.getElementById('stores-list');
+    let data = "8480 Beverly Blvd Los Angeles, CA 90048"
+
+    for(let i = 0; i<4; i++) {
+        let storeContainer = document.createElement("div");
+        storeContainer.className = "store-container";
+
+        let addressDiv = document.createElement("div");
+        addressDiv.className = "store-address";
+        addressDiv.innerHTML = data;
+
+        let phoneDiv = document.createElement("div");
+        phoneDiv.className = "store-phone-number";
+        phoneDiv.innerHTML = "9999-9999-9999";
+
+        storeContainer.appendChild(addressDiv);
+        storeContainer.appendChild(phoneDiv);
+        storeList.appendChild(storeContainer);
+    }
 }
--- readme.md
+++ readme.md
@@ -1,13 +1,14 @@
 ## Google Locator Application(Coding Challenge)
 Given the 6 figure Javascript Fullstack coding challenge from Clever Programmer I created this repository.
 
-## Steps followed in the course
-- 1
-- 2
-- 3
-- 4
-- 5
-- 6
+## Plan of Action
+- Create Wireframe
+- Set Up the base HTML structure
+- Import Google Maps into the Window
+- Add the text Store Locator
+- Add input box
+- Add Store List Container
+- Add individual Store Container
 
 ## Results
 
