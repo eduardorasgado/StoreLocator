9e5fcfa1b120ae3125f5f1a941401b141b3e3706
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2020-04-09 01:24:12 -0500

    elements within the list renders depending on input field

--- js/index.js
+++ js/index.js
@@ -3,12 +3,32 @@ var la = {
     lng: -118.243683
 };
 
+var markersList = [];
 var map;
 // to be able to store the markers and remove them globally
 var mapMarker;
 // mapBound should be global to fix it at the markers location
 var mapBoundListener;
+var currentStores;
+var searchInput;
 
+window.onload = () => {
+    // loading and storing all the stores within the store data
+    //currentStores = getAddressesByZipCode();
+
+    searchInput = document.getElementById("zip-code-input");
+    searchInput.addEventListener("keyup",function(event) {
+        // number 13 is enter
+        if(event.keyCode == 13) {
+            event.preventDefault();
+            loadScreenDataByZipCode();
+        }
+    })
+}
+
+/**
+ * First time google maps load
+ */
 function initMap() {
   // The location of Uluru
   //var uluru = {lat: -25.344, lng: 131.036};
@@ -16,16 +36,48 @@ function initMap() {
   var map = new google.maps.Map(
       document.getElementById('map'), {center: la, zoom:14});
   // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: la, map: map});
+  var marker = new google.maps.Marker({position: la, map: map});  
+
+  // listing all the stores inmediately the page has loaded
+  currentStores = getAddressesByZipCode();
   addressesLoader();
+}
+
+function loadScreenDataByZipCode() {
+    let zipCode = searchInput.value;
+    if(zipCode) {
+        currentStores = getAddressesByZipCode(zipCode);
+        addressesLoader();
+        //addMapMarkers();
+    }
+}
+
+/**
+ * This function returns an array of stores given a certain zipCode
+ * @param {*} zipCode 
+ */
+function getAddressesByZipCode(zipCode = '') {
+    if(zipCode == '') {
+        return stores;
+    } 
+    let storesToReturn = [];
+    let storeZipCode;
+    for(let store of stores) {
+        // assuming every call to the api, data comes clean
+        // with this field in every store
+        storeZipCode = store.address.postalCode.slice(0, 5);
+        if(storeZipCode === zipCode) storesToReturn.push(store);
+    }
+    return storesToReturn;
 }
 
 /**
  * inserting every location within store data within the store list
  */
 function addressesLoader() {
-    
     let storeList = document.getElementById('stores-list');
+    // cleaning the store list
+    storeList.innerHTML = '';
     let data;
     let storeContainer;
     let addressDiv;
@@ -34,17 +86,10 @@ function addressesLoader() {
     let hr;
     let circle;
     let cicleSpan;
-    let markers = []
-    for(let store of stores) {
+    for(let store of currentStores) {
+        //console.log(store)
         // rendering the first 4 stores, for the moment, this will be dynamic
-        if(i === 4) break;
         data = `${store.addressLines[0]} <br>${store.addressLines[1]}`
-        
-        // appending all the coordinates from stores
-        markers.push([store.brandName,
-            store.coordinates["latitude"],
-            store.coordinates["longitude"]]
-            )
 
         storeContainer = document.createElement("div");
         storeContainer.className = "store-container";
@@ -71,15 +116,18 @@ function addressesLoader() {
         storeList.appendChild(storeContainer);
         ++i;
     }
-
-    // rendering all the markers within the map
-    addMapMarkers(markers);
 }
 
 
 // google maps markers appending
 function addMapMarkers(markers) {
-    // TODO: Check to create a map list or just remove all the markers into the 
+    // appending all the coordinates from stores
+    //markersList.push([store.brandName,
+    //    store.coordinates["latitude"],
+    //    store.coordinates["longitude"]]
+    //    )
+    
+        // TODO: Check to create a map list or just remove all the markers into the 
     // marker object
     mapMarker = null;
     // to be able to load information into the marker on click
